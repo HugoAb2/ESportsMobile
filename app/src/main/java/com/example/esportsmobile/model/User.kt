b194a720afa7177c4219ca8ba95053d3890f57f2
HugoAb2 <rodrigueshugoab2@gmail.com>
2022-11-09 20:52:51 -0300

    update: implementing Serializable to User.kt

--- app/src/main/java/com/example/esportsmobile/model/User.kt
+++ app/src/main/java/com/example/esportsmobile/model/User.kt
@@ -1,9 +1,11 @@
 package com.example.esportsmobile.model
 
+import java.io.Serializable
+
 data class User(
     var name : String,
     var age : Int,
     var country : String,
     var email : String,
     var password : String,
-)
+): Serializable
