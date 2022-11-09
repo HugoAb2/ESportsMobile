9a9bd68bc695e10ab1dbac8c21041a8a48741b36
HugoAb2 <rodrigueshugoab2@gmail.com>
2022-11-09 19:02:53 -0300

    update: guarding data received into a local variable

--- app/src/main/java/com/example/esportsmobile/LeagueActivity.kt
+++ app/src/main/java/com/example/esportsmobile/LeagueActivity.kt
@@ -2,10 +2,21 @@ package com.example.esportsmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.esportsmobile.databinding.ActivityLeagueBinding
 
 class LeagueActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLeagueBinding
+    private lateinit var bundle : Bundle
+
+    private lateinit var leagueName : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_league)
+        binding = ActivityLeagueBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bundle = intent.extras!!
+        leagueName = bundle.getString("league_name").toString()
     }
 }
