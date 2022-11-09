aa74bea993819174a413edb3536094839a3dd88c
HugoAb2 <rodrigueshugoab2@gmail.com>
2022-11-09 19:01:33 -0300

    add: create dataclass for User

---
+++ app/src/main/java/com/example/esportsmobile/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.esportsmobile.model
+
+data class User(
+    var name : String,
+    var age : Int,
+    var country : String,
+    var email : String,
+    var password : String,
+)
