d86b94580caf345a47f13541fec51b51b32a6ade
HugoAb2 <rodrigueshugoab2@gmail.com>
2022-11-17 22:05:45 -0300

    add: created EditProfileActivity.kt

--- app/src/main/java/com/example/esportsmobile/DrawerBaseActivity.kt
+++ app/src/main/java/com/example/esportsmobile/DrawerBaseActivity.kt
@@ -1,12 +1,15 @@
 package com.example.esportsmobile
 
+import android.app.Activity
 import android.content.Intent
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -15,8 +18,28 @@ import com.google.android.material.navigation.NavigationView
 
 open class DrawerBaseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
 
+    companion object{
+        private const val TAG = "editActivity"
+    }
+
+
     private lateinit var drawerLayout : View
     private lateinit var navViewGlobal : NavigationView
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == Activity.RESULT_OK){
+            Log.d(TAG, "updatedUserReceive: ${it.data?.getSerializableExtra("user")as User}")
+            val header = navViewGlobal.getHeaderView(0)
+            val nUser = (it.data?.getSerializableExtra("user") as User)
+            header.apply {
+                findViewById<TextView>(R.id.user_name).text = nUser.name
+                findViewById<TextView>(R.id.user_email).text = nUser.email
+            }
+        }
+        if (it.resultCode == Activity.RESULT_CANCELED){
+            Toast.makeText(this, "Changes not applied", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun setContentView(view: View?) {
         drawerLayout = layoutInflater.inflate(R.layout.activity_drawer_base, null)
@@ -42,11 +65,6 @@ open class DrawerBaseActivity : AppCompatActivity(), NavigationView.OnNavigation
         }
     }
 
-    protected fun allocateActivityTittle(activityTittle:String){
-        val tittle = findViewById<TextView>(R.id.page_name_indicator)
-        tittle.text = activityTittle
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId){
             R.id.change_account -> {
@@ -58,7 +76,10 @@ open class DrawerBaseActivity : AppCompatActivity(), NavigationView.OnNavigation
                 startActivity(intent)
             }
             R.id.update_profile ->{
-                Toast.makeText(this, "Works", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, EditProfileActivity::class.java)
+                var email = navViewGlobal.getHeaderView(0).findViewById<TextView>(R.id.user_email).text
+                intent.putExtra("user_email", email)
+                launcher.launch(intent)
             }
         }
         return true
@@ -75,7 +96,11 @@ open class DrawerBaseActivity : AppCompatActivity(), NavigationView.OnNavigation
             findViewById<TextView>(R.id.user_name).text = user.name
             findViewById<TextView>(R.id.user_email).text = user.email
         }
-
-
     }
+
+    protected fun allocateActivityTittle(activityTittle:String){
+        val tittle = findViewById<TextView>(R.id.page_name_indicator)
+        tittle.text = activityTittle
+    }
+
 }
